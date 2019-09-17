f8c3dd7d417d65987e8ea28ca6c90b4e4d7179ab
Keshav Chawla <hello@keshavchawla.com>
2019-09-17 12:54:34 -0400

    --

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 $('.faded-div').fadeIn(0);
 
-    $('.faded-div').delay(500).fadeOut(500);
+    $('.faded-div').delay(300).fadeOut(500);
 
 $('a.fade-out').click(function(){
     $('.faded-div').fadeOut(1000);
